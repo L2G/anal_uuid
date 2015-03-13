4b80cffa61c6d1dae2e700008c70b6ae9cc281ed
Larry Gilbert <larry@L2G.to>
2015-03-12 17:20:54 -0700

    Add some notes to main

--- lib/anal_uuid.rb
+++ lib/anal_uuid.rb
@@ -1,5 +1,18 @@
 require 'anal_uuid/version'
 
+# Parse a UUID as defined in IETF RFC 4122, "A Universally Unique IDentifier
+# (UUID) URN Namespace" by Leach, Mealling, and Salz
+# (http://www.ietf.org/rfc/rfc4122.txt)
+#
+# Other references:
+#
+# "Universal Unique Identifier". The Open Group. CDE 1.1: Remote Procedure Call
+# (http://pubs.opengroup.org/onlinepubs/9629399/apdxa.htm).
+#
+# "Privilege (Authorisation) Services." The Open Group. DCE 1.1: Authentication
+# and Security Services
+# (http://pubs.opengroup.org/onlinepubs/9668899/chap5.htm#tagcjh_08_02_01_01).
+#
 module AnalUUID
   # Your code goes here...
 end
